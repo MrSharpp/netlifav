f7c4ccbb1d7f25337b1e71bf00773c965a637492
Amir Alam <40666775+MrSharpp@users.noreply.github.com>
2023-11-01 19:16:13 +0530

    static assets serving

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { config } from "@configs/config";
 import { routerV1 } from "@routes";
+import path from "path";
 
 const app = express();
+
+app.use("/public", express.static(path.join(__dirname, "..", "public")));
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
