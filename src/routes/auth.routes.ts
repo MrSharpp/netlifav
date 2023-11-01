7055b33022fafb414a714c3bad4518dc8f0f2baf
Amir Alam <40666775+MrSharpp@users.noreply.github.com>
2023-11-01 19:31:56 +0530

    auth method routes added

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -3,8 +3,12 @@ import expres from "express";
 
 const authRouter = expres.Router();
 
-authRouter.get("/register", AuthController.loginPage);
-
+// render pages
+authRouter.get("/login", AuthController.loginPage);
 authRouter.get("/register", AuthController.registerPage);
+
+// methods
+authRouter.post("/login", AuthController.loginUser);
+authRouter.post("/register", AuthController.registerPage);
 
 export default authRouter;
