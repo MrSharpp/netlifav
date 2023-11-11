6f5d46c364ff35cd6d11395a20aa5ccd661027fa
Amir Alam <40666775+MrSharpp@users.noreply.github.com>
2023-11-11 20:12:11 +0530

    Revert "changes"
    This reverts commit 0088fde280082cf494c677c920ffb7a4edb9c399.

--- Netlifav.Client/src/schema/create-actor-genre.schema.ts
+++
@@ -1,5 +0,0 @@
-import z from "zod";
-
-export const CreateActorOrGenreSchema = z.object({
-  name: z.string().min(1),
-});
--- Netlifav.Client/src/schema/create-movie.schema.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod";
-
-export const CreateMovieSchema = z.object({
-  name: z.string().min(1),
-  rating: z.coerce.number(),
-  genre: z.string().uuid(),
-  casts: z.array(z.string().uuid()).min(1),
-  releaseDate: z.coerce.date(),
-});
